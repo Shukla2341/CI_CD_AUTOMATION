229bc5b145b60cc7edae131f3b79c36c22941dac
Shukla213 <aashutoshshukla985@gmail.com>
2026-04-29 14:50:29 +0530

    this is correct change

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Deployment Workin 🚀");
+    res.send("Deployment Working 🚀");
 });
 
 module.exports = app;
